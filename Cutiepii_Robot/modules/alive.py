36b56b3bda7d30d318d60a12b7b10bae560dea48
[ Vicky | JR ] <90129845+Vickyftw@users.noreply.github.com>
2022-05-31 10:42:59 +0530

    Update alive.py

--- Cutiepii_Robot/modules/alive.py
+++ Cutiepii_Robot/modules/alive.py
@@ -41,17 +41,17 @@ from pyrogram import __version__ as pyrover
 
 edit_time = 5
 """ =======================Hottie====================== """
-file1 = "https://telegra.ph/file/11cfb0be7163d32c51259.jpg"
-file2 = "https://telegra.ph/file/444028d9b3daccc947a2d.jpg"
-file3 = "https://telegra.ph/file/fdf47498b208bc63000b4.jpg"
-file4 = "https://telegra.ph/file/e8f3310b943b8b8699dcd.jpg"
-file5 = "https://telegra.ph/file/401cb7f6216764ebab161.jpg"
+file1 = "https://telegra.ph/file/b6b6d37ef00603df9bd99.jpg"
+file2 = "https://telegra.ph/file/f1912776dda151964fd53.jpg"
+file3 = "https://telegra.ph/file/43fb7a717d1d4acce391c.jpg"
+file4 = "https://telegra.ph/file/c641abcfbd0169b7ed48e.jpg"
+file5 = "https://telegra.ph/file/29066bcd398b0725199a0.jpg"
 """ =======================Hottie====================== """
 
 BUTTON = [
     [
         Button.url("【► HELP ◄】", "https://t.me/Cutiepii_Robot?start=help"),
-        Button.url("【► SUPPORT ◄】", "https://t.me/Black_Knights_Union_Support"),
+        Button.url("【► SUPPORT ◄】", "https://t.me/angelsupports"),
     ]
 ]
 
@@ -84,16 +84,16 @@ async def hmm(yes):
     current_time = datetime.utcnow()
     uptime_sec = (current_time - START_TIME).total_seconds()
     uptime = await _human_time_duration(int(uptime_sec))
-    pm_caption = f"** ♡ Hey [{yes.sender.first_name}](tg://user?id={yes.sender.id}) I,m Cutiepii 愛 **\n\n"
+    pm_caption = f"** ♡ Hey [{yes.sender.first_name}](tg://user?id={yes.sender.id}) I,m Angel 天使 **\n\n"
     pm_caption += f"**♡ My Uptime :** `{uptime}`\n\n"
     pm_caption += f"**♡ Python Version :** `{python_version}`\n\n"
     pm_caption += f"**♡ Telethon Version :** `{tlhver}`\n\n"
     pm_caption += f"**♡ Pyrogram Version :** `{pyrover}`\n\n"
-    pm_caption += "**♡ My Master :** [Rajkumar](https://t.me/Awesome_RJ_official) "
+    pm_caption += "**♡ My Master :** [Mr.Shelby](https://t.me/Lost_Kiddo) "
     BUTTON = [
         [
             Button.url("【► Help ◄】", "https://t.me/Cutiepii_Robot?start=help"),
-            Button.url("【► Support ◄】", "https://t.me/Black_Knights_Union_Support"),
+            Button.url("【► Support ◄】", "https://t.me/angelsupports"),
         ]
     ]
     on = await borg.send_file(
